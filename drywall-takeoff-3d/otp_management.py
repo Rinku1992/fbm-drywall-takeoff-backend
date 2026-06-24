aae45f4fcd12a08e39a76380fe5e26fd805f9dec
Debajyoti-R <debajyoti.r@myfbm.com>
2026-06-25 00:27:46 +0530

    Rename generate_otp_code to generate_otp

--- drywall-takeoff-3d/otp_management.py
+++ drywall-takeoff-3d/otp_management.py
@@ -41,7 +41,7 @@ def is_valid_email(email):
     pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
     return re.match(pattern, email) is not None
 
-def generate_otp_code():
+def generate_otp():
     return "".join(random.choices(string.digits, k=6))
 
 def create_external_login_jwt(credentials, user_id):
