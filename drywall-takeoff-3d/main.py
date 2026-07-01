ad1464fe88bfd2591bfa166692e942781c809ed5
Debajyoti-R <debajyoti.r@myfbm.com>
2026-07-01 07:00:07 +0530

    Update file paths for GCP credentials and hyperparameters

--- drywall-takeoff-3d/main.py
+++ drywall-takeoff-3d/main.py
@@ -576,7 +576,7 @@ def enable_logging_on_stdout():
 
 def load_gcp_credentials() -> dict:
     yaml = YAML(typ="safe", pure=True)
-    with open("gcp.yaml", 'r') as f:
+    with open("config/gcp.yaml", 'r') as f:
         credentials = yaml.load(f)
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials["service_drywall_account_key"]
 
@@ -585,7 +585,7 @@ def load_gcp_credentials() -> dict:
 
 def load_hyperparameters() -> dict:
     yaml = YAML(typ="safe", pure=True)
-    with open("hyperparameters.yaml", 'r') as f:
+    with open("config/hyperparameters.yaml", 'r') as f:
         hyperparameters = yaml.load(f)
 
     return hyperparameters
