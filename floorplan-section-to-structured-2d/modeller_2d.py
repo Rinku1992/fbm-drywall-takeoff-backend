fb91a899d9ca06e05f931092cfaa87422d9301b2
Debajyoti-R <debajyoti.r@myfbm.com>
2026-07-05 10:40:04 +0530

    Increase ThreadPoolExecutor max_workers to 8

--- floorplan-section-to-structured-2d/modeller_2d.py
+++ floorplan-section-to-structured-2d/modeller_2d.py
@@ -588,7 +588,7 @@ class FloorPlan2D(FloorPlan):
             shapes = self.disconnected_shapes(lines)
 
             is_valid_futures = list()
-            with ThreadPoolExecutor(max_workers=4) as executor:
+            with ThreadPoolExecutor(max_workers=8) as executor:
                 for shape in shapes:
                     is_valid_futures.append(executor.submit(
                         self._is_shape_valid,
@@ -2762,7 +2762,7 @@ class FloorPlan2D(FloorPlan):
             return True
 
         walls_null_room, walls_null_id, is_valid_futures = list(), list(), list()
-        with ThreadPoolExecutor(max_workers=4) as executor:
+        with ThreadPoolExecutor(max_workers=8) as executor:
             for wall in walls_2d:
                 if wall["polygons_drywall"][0]["type"] == wall["polygons_drywall"][1]["type"] == "DISABLED":
                     wall_line = [[wall["wall_line"][0]['x'], wall["wall_line"][0]['y'], wall["wall_line"][1]['x'], wall["wall_line"][1]['y']]]
@@ -2984,7 +2984,7 @@ class FloorPlan2D(FloorPlan):
         perimeter_lines, outer_drywall_surfaces = self.perimeter_lines(wall_lines)
         polygon_vertices_normalized_all = list()
         futures = list()
-        with ThreadPoolExecutor(max_workers=4) as executor:
+        with ThreadPoolExecutor(max_workers=8) as executor:
             for index, ((polygon_area, polygon_vertices), polygon_perimeter_walls) in enumerate(zip(polygons, polygons_perimeter_walls)):
                 index += 1
                 polygon_vertices_normalized = [(round(scale_x * vertex[0]), round(scale_y * vertex[1])) for vertex in polygon_vertices]
@@ -3047,7 +3047,7 @@ class FloorPlan2D(FloorPlan):
         )
         external_contour_normalized = self.merge_polygons(external_contour_normalized, [polygon[1] for polygon in missing_polygons])
         futures = list()
-        with ThreadPoolExecutor(max_workers=4) as executor:
+        with ThreadPoolExecutor(max_workers=8) as executor:
             for index, ((polygon_area, polygon_vertices), polygon_perimeter_walls) in enumerate(zip(missing_polygons, missing_polygons_perimeter_walls)):
                 index += len(polygons)
                 drywall_polygons = self._extrude_polygon_drywalls(polygon_perimeter_walls, polygon_vertices, (scale_x, scale_y))
@@ -3118,7 +3118,7 @@ class FloorPlan2D(FloorPlan):
         perimeter_lines, outer_drywall_surfaces = self.perimeter_lines(wall_lines)
         polygon_vertices_normalized_all = list()
         futures = list()
-        with ThreadPoolExecutor(max_workers=4) as executor:
+        with ThreadPoolExecutor(max_workers=8) as executor:
             for index, ((polygon_area, polygon_vertices), polygon_perimeter_walls) in enumerate(zip(polygons, polygons_perimeter_walls)):
                 index += 1
                 polygon_vertices_normalized = [(round(scale_x * vertex[0]), round(scale_y * vertex[1])) for vertex in polygon_vertices]
@@ -3179,7 +3179,7 @@ class FloorPlan2D(FloorPlan):
         )
         external_contour_normalized = self.merge_polygons(external_contour_normalized, [polygon[1] for polygon in missing_polygons])
         futures = list()
-        with ThreadPoolExecutor(max_workers=4) as executor:
+        with ThreadPoolExecutor(max_workers=8) as executor:
             for index, ((polygon_area, polygon_vertices), polygon_perimeter_walls) in enumerate(zip(missing_polygons, missing_polygons_perimeter_walls)):
                 index += len(polygons)
                 drywall_polygons = self._extrude_polygon_drywalls(polygon_perimeter_walls, polygon_vertices, (scale_x, scale_y))
@@ -3217,7 +3217,7 @@ class FloorPlan2D(FloorPlan):
                 self._walls_2d, self._polygons = json.load(f)
 
         futures = list()
-        with ThreadPoolExecutor(max_workers=4) as executor:
+        with ThreadPoolExecutor(max_workers=8) as executor:
             for polygon in self._polygons:
                 futures.append(executor.submit(
                     self._add_drywalls_polygon,
