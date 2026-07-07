00281904dfa599139380e0d75b37ac9267279c64
Debajyoti-R <debajyoti.r@myfbm.com>
2026-07-07 20:14:51 +0530

    Add TransportError to exception handling

--- xtimator-3d/helper.py
+++ xtimator-3d/helper.py
@@ -208,6 +208,7 @@ def pg_run(
             InterfaceError,
             InterfaceErrorPG8000,
             TimeoutError,
+            TransportError,
         ) as e:
 
             if conn is not None:
