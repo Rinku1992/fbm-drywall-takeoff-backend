3ecfee185184efc6ece07a183fc46ddece994edd
Debajyoti-R <debajyoti.r@myfbm.com>
2026-07-09 06:12:05 +0530

    Update main.py

--- floorplan-to-structured-2d/main.py
+++ floorplan-to-structured-2d/main.py
@@ -46,7 +46,6 @@ from helper import (
     pg_run,
     load_subscriber_client,
     query_subscriber_messages,
-    create_session,
     is_session_active,
     terminate_session,
 )
@@ -290,11 +289,11 @@ async def floorplan_to_structured_2d(request: Request):
     elevation_pages = parameters.get("elevation_pages") or body.get("elevation_pages")
     predict_drywall = parameters.get("predict_drywall") or body.get("predict_drywall") or "true"
     architectural_scale = parameters.get("architectural_scale") or body.get("architectural_scale")
+    session_uuid = parameters.get("session_uuid") or body.get("session_uuid")
     page_number = int(page_number)
     predict_drywall = predict_drywall.upper() == "TRUE"
     logging.info("SYSTEM: Received a Floorplan 2D Model Generation Request")
 
-    session_uuid = await create_session(CREDENTIALS, pg_pool, project_id, plan_id, user_id, page_number)
     query = f"SELECT user_id FROM {CREDENTIALS["CloudSQL"]["table_name_plans"]} WHERE LOWER(project_id) = LOWER(%s) AND LOWER(plan_id) = LOWER(%s)"
     user_id_owner = await run_in_threadpool(partial(pg_run, CREDENTIALS, pg_pool, query, params=(project_id, plan_id,), fetch=True))
     if user_id_owner:
