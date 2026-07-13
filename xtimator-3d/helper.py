4da17a072177749e0869135d8737c3a475656b1a
Debajyoti-R <debajyoti.r@myfbm.com>
2026-07-14 00:29:38 +0530

    Enhance load_templates to prioritize drywall SKUs
    Filter and sort product templates based on primary drywall SKUs.

--- xtimator-3d/helper.py
+++ xtimator-3d/helper.py
@@ -496,6 +496,8 @@ def load_floorplan_to_preview_ID_token(credentials):
     return id_token
 
 async def load_templates(pg_pool, credentials):
+    drywall_skus_primary = ["D12L", "D12LW", "D12MM", "D58F", "D12GMTB", "DCB12"]
+    drywall_sku_order = {sku: index for index, sku in enumerate(drywall_skus_primary)}
     query = f"SELECT * FROM {credentials["CloudSQL"]["table_name_sku"]}"
     product_templates = await run_in_threadpool(partial(pg_run, credentials, pg_pool, query, fetch=True))
 
@@ -510,7 +512,12 @@ async def load_templates(pg_pool, credentials):
         product_template["sku_variant"] = f"{product_template["sku_id"]} - {product_template["sku_description"]}"
         product_template["color_code"] = [product_template["color_code"]['b'], product_template["color_code"]['g'], product_template["color_code"]['r']]
         product_templates_target.append(product_template)
-    return product_templates_target
+
+    product_templates_target_primary = list(filter(lambda template: template["sku_id"] in drywall_skus_primary, product_templates_target))
+    product_templates_target_primary.sort(key=lambda template: drywall_sku_order.get(template["sku_id"], len(product_templates_target)))
+    [product_templates_target.remove(template_primary) for template_primary in product_templates_target_primary]
+    product_templates_target = product_templates_target_primary + product_templates_target
+    return jsonable_encoder(product_templates_target)
 
 def query_drywall(query_sku_variant, drywall_templates):
     for drywall_template in drywall_templates:
