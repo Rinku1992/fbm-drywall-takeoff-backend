57b7b6bd1e42291d8d3cde22b5e01e0cd34313ef
Debajyoti-R <debajyoti.r@myfbm.com>
2026-07-15 01:31:36 +0530

    Update main.py

--- xtimator-3d/main.py
+++ xtimator-3d/main.py
@@ -2113,6 +2113,13 @@ async def update_floorplan_to_2d(request: Request):
     resolution_scale = (scale_x, scale_y,)
 
     polygons_JSON = plan.reshape_polygons(polygons_JSON, walls_2d_JSON, architectural_scale=scale, resolution_scale=resolution_scale)
+    polygons_JSON_sharded = list()
+    polygon_idx = 0
+    for polygon in polygons_JSON[:]
+        perimeter_lines_contour = plan.load_perimeter(polygon["vertices"], wall_lines, scale=resolution_scale)
+        polygons_sharded = plan.shard_polygon(polygon["vertices"], perimeter_lines_contour, scale, resolution_scale):
+        for polygon in polygons_sharded:
+            
     for polygon in polygons_JSON[:]:
         perimeter_lines_contour = plan.load_perimeter(polygon["vertices"], wall_lines, scale=resolution_scale)
         perimeter_wall_line_ids = [wall_line_ids[wall_lines.index(perimeter_line_contour)] for perimeter_line_contour in perimeter_lines_contour]
