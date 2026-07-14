2e95ff9d9dd679c80929131241c55cc6985d6c2e
Debajyoti-R <debajyoti.r@myfbm.com>
2026-07-15 01:40:33 +0530

    Refactor polygon processing to include area calculations

--- xtimator-3d/main.py
+++ xtimator-3d/main.py
@@ -2118,8 +2118,14 @@ async def update_floorplan_to_2d(request: Request):
     for polygon in polygons_JSON[:]
         perimeter_lines_contour = plan.load_perimeter(polygon["vertices"], wall_lines, scale=resolution_scale)
         polygons_sharded = plan.shard_polygon(polygon["vertices"], perimeter_lines_contour, scale, resolution_scale):
-        for polygon in polygons_sharded:
-            
+        for (polygon_area_shoelace, area, vertices) in polygons_sharded:
+            polygon["id"] = polygon_idx
+            polygon["area"] = area
+            polygon["polygon_area_shoelace"] = polygon_area_shoelace
+            polygon["vertices"] = vertices
+            polygons_JSON_sharded.append(polygon)
+            polygon_idx += 1
+    polygons_JSON = polygons_JSON_sharded
     for polygon in polygons_JSON[:]:
         perimeter_lines_contour = plan.load_perimeter(polygon["vertices"], wall_lines, scale=resolution_scale)
         perimeter_wall_line_ids = [wall_line_ids[wall_lines.index(perimeter_line_contour)] for perimeter_line_contour in perimeter_lines_contour]
