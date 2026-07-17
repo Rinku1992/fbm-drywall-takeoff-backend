d70fbdf024a0b2468ce8ff69fa0895819437f093
Debajyoti-R <debajyoti.r@myfbm.com>
2026-07-17 23:16:59 +0530

    Fix list comprehension for visible users

--- xtimator-3d/policy.py
+++ xtimator-3d/policy.py
@@ -66,5 +66,5 @@ class AccessControlService:
             ORDER BY u.user_email;
         """
         visible_users = await run_in_threadpool(partial(pg_run, self._credentials, self._pg_pool, query, params=(user_id,), fetch=True))
-        visible_users = [visible_users["user_email"] for visible_user in visible_users]
+        visible_users = [visible_user["user_email"] for visible_user in visible_users]
         return visible_users
