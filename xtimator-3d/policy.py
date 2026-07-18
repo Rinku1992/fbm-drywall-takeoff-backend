06fc76c35453fbbaab618b941a6100fb203155e0
Debajyoti-R <debajyoti.r@myfbm.com>
2026-07-18 22:11:57 +0530

    Implement _is_owner method for project ownership check
    Add asynchronous method to check project ownership.

--- xtimator-3d/policy.py
+++ xtimator-3d/policy.py
@@ -43,6 +43,14 @@ class AccessControlService:
         self._credentials = credentials
         self._pg_pool = pg_pool
 
+    async def _is_owner(self, project_id, plan_id, page_number, user_id):
+        query = f"""
+            SELECT user_id fom {self._credentials["CloudSQL"]["table_name_models"]}
+            WHERE LOWER(project_id) = LOWER(%s) AND LOWER(plan_id) = LOWER(%s) AND LOWER(page_number) = LOWER(%s)
+        """
+        user_owner = await run_in_threadpool(partial(pg_run, self._credentials, self._pg_pool, query, params=(project_id, plan_id, page_number,), fetch=True))
+        return user_id.lower() == user_owner.lower()
+
     async def load_scope(self, user_id):
         query = f"""
             SELECT
@@ -56,7 +64,6 @@ class AccessControlService:
                 ON p.permission_id = rp.permission_id
             WHERE LOWER(u.user_email) = LOWER(%s);
         """
-        await run_in_threadpool(partial(pg_run, self._credentials, self._pg_pool, query, params=(user_id,), fetch=True))
         user_scopes = await run_in_threadpool(partial(pg_run, self._credentials, self._pg_pool, query, params=(user_id,), fetch=True))
         user_scopes = [user_scope["permission_name"] for user_scope in user_scopes]
         return Scope(user_scopes)
