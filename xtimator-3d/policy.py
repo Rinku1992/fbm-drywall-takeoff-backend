03f3b175f0f987570a74818b466633062665e594
Debajyoti-R <debajyoti.r@myfbm.com>
2026-07-18 23:06:21 +0530

    Fix SQL syntax error in _is_owner method

--- xtimator-3d/policy.py
+++ xtimator-3d/policy.py
@@ -45,8 +45,8 @@ class AccessControlService:
 
     async def _is_owner(self, project_id, plan_id, page_number, user_id):
         query = f"""
-            SELECT user_id fom {self._credentials["CloudSQL"]["table_name_models"]}
-            WHERE LOWER(project_id) = LOWER(%s) AND LOWER(plan_id) = LOWER(%s) AND LOWER(page_number) = LOWER(%s)
+            SELECT user_id FROM {self._credentials["CloudSQL"]["table_name_models"]}
+            WHERE LOWER(project_id) = LOWER(%s) AND LOWER(plan_id) = LOWER(%s) AND LOWER(page_number) = LOWER(%s);
         """
         user_owner = await run_in_threadpool(partial(pg_run, self._credentials, self._pg_pool, query, params=(project_id, plan_id, page_number,), fetch=True))
         return user_id.lower() == user_owner.lower()
