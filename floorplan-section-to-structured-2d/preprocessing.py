1e7ea418ce1b6c099b5566b39c104ab22399ef1b
Debajyoti-R <debajyoti.r@myfbm.com>
2026-07-25 04:53:25 +0530

    Refactor image processing with DPI handling

--- floorplan-section-to-structured-2d/preprocessing.py
+++ floorplan-section-to-structured-2d/preprocessing.py
@@ -2,16 +2,41 @@ from pathlib import Path
 from pdf2image import convert_from_path
 import cv2
 from PIL import Image
-Image.MAX_IMAGE_PIXELS = None
+Image.MAX_IMAGE_PIXELS = 200000000
 
 
-def process_page(pdf_path, page_index, image_path_page, dpi):
+def process_page(pdf_path, page_index, image_path_page, maximum_dpi, minimum_dpi, dpi_reduction_factor: float=0.75):
     pdf_page = convert_from_path(
         pdf_path,
         dpi=dpi,
         first_page=page_index+1,
         last_page=page_index+1
     )[0]
+    current_dpi = maximum_dpi
+
+    while True:
+        try:
+            pdf_page = convert_from_path(
+                pdf_path,
+                dpi=current_dpi,
+                first_page=page_index+1,
+                last_page=page_index+1,
+            )[0]
+            break
+
+        except Image.DecompressionBombError:
+            if current_dpi <= minimum_dpi:
+                logging.error(
+                    "DecompressionBombError persists at minimum DPI=%s",
+                    minimum_dpi,
+                )
+                raise
+
+            new_dpi = max(
+                minimum_dpi,
+                int(current_dpi * dpi_reduction_factor)
+            )
+            current_dpi = new_dpi
     save(pdf_page, image_path_page)
     to_sharp(image_path_page)
     del pdf_page
@@ -33,9 +58,9 @@ def to_sharp(image_path_page):
     cv2.imwrite(output_path, sharpened)
     return sharpened
 
-def preprocess(pdf_path, page_index, dpi=400, image_path="/tmp/floor_plan.png"):
+def preprocess(pdf_path, page_index, maximum_dpi=400, minimum_dpi=250, image_path="/tmp/floor_plan.png"):
     image_path = Path(image_path)
     image_path_page = image_path.parent.joinpath(image_path.stem).with_suffix(f".{str(page_index).zfill(2)}{image_path.suffix}")
-    process_page(pdf_path, page_index, image_path_page, dpi)
+    process_page(pdf_path, page_index, image_path_page, maximum_dpi, minimum_dpi)
 
     return image_path_page
