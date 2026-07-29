1826f467d6ddc921b7adce83b4fdc560fc6262ea
Debajyoti-R <debajyoti.r@myfbm.com>
2026-07-29 19:07:39 +0530

    Add DPI parameters to preprocess function

--- xtimator-3d/preprocessing.py
+++ xtimator-3d/preprocessing.py
@@ -37,9 +37,9 @@ def process_page(pdf_path, page_index, image_path_page, maximum_dpi, minimum_dpi
 def save(pdf_page, image_path_page):
     pdf_page.save(image_path_page, "PNG")
 
-def preprocess(pdf_path, page_index, image_path="/tmp/floor_plan.png"):
+def preprocess(pdf_path, page_index, maximum_dpi=400, minimum_dpi=250, image_path="/tmp/floor_plan.png"):
     image_path = Path(image_path)
     image_path_page = image_path.parent.joinpath(image_path.stem).with_suffix(f".{str(page_index).zfill(2)}{image_path.suffix}")
-    _ = process_page(pdf_path, page_index, image_path_page)
+    _ = process_page(pdf_path, page_index, image_path_page, maximum_dpi, minimum_dpi)
 
     return image_path_page
