3bc554e84b956fd68c1e03bdefdbcc94f29f3999
Debajyoti-R <debajyoti.r@myfbm.com>
2026-08-03 00:02:55 +0530

    Change metadata to empty dict in helper.py

--- floorplan-section-to-structured-2d/helper.py
+++ floorplan-section-to-structured-2d/helper.py
@@ -605,8 +605,8 @@ async def insert_model_2d(
     target_drywalls,
     pg_pool,
     credentials,
-    model_2d=dict(walls_2d=list(), polygons=list(), metadata=metadata),
-    layout_2d=dict(walls_2d=list(), polygons=list(), metadata=metadata),
+    model_2d=dict(walls_2d=list(), polygons=list(), metadata=dict()),
+    layout_2d=dict(walls_2d=list(), polygons=list(), metadata=dict()),
     insert_model=False,
     insert_layout=False,
     ):
