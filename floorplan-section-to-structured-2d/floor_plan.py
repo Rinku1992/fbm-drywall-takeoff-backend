43f564b20fe4ffa7fb7a366a2afbf80b5412bb1c
Debajyoti-R <debajyoti.r@myfbm.com>
2026-08-03 06:53:13 +0530

    Update DPI handling in compute_imperial_scale_from_DPI

--- floorplan-section-to-structured-2d/floor_plan.py
+++ floorplan-section-to-structured-2d/floor_plan.py
@@ -46,10 +46,11 @@ class FloorPlan:
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         return gray
 
-    def compute_imperial_scale_from_DPI(self, architectural_scale_normalized):
-        DPI = self.hyperparameters["modelling"]["scale_adoption"]["dpi"]["in_use"]
+    def compute_imperial_scale_from_DPI(self, architectural_scale_normalized, DPI=None):
+        if not DPI:
+            DPI = self.hyperparameters["modelling"]["scale_adoption"]["dpi"]["in_use"]
         scale_on_paper_length = float(architectural_scale_normalized.split(':')[0].strip('`'))
-        imperial = DPI * scale_on_paper_length
+        imperial = int(DPI) * scale_on_paper_length
         return (1 / imperial, 1 / imperial)
 
     @classmethod
