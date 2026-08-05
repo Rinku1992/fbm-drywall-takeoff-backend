f2102c3e636b1d544b6a38e32f6a2ca8961e87b6
Debajyoti-R <debajyoti.r@myfbm.com>
2026-08-05 19:37:54 +0530

    Update main.py

--- xtimator-3d/main.py
+++ xtimator-3d/main.py
@@ -784,6 +784,10 @@ async def load_projects(request: Request):
             peers = await access_control.load_organization_users(user_id)
             where_clause = "LOWER(p.created_by) = ANY(%s)"
             params = (peers,)
+            peers_partner = await access_control.load_regional_users_partner_organizations(user_id)
+            region_names = await access_control.load_user_region_names(user_id)
+            where_clause_partner = "LOWER(p.created_by) = ANY(%s) AND LOWER(p.\"FBM_branch\") = ANY(%s)"
+            params_partner = (peers_partner, region_names,)
 
     else:
         peers = await access_control.load_regional_users(user_id)
