390be5689cd370a7c118d7e76aabc81ed8bde378
Debajyoti-R <debajyoti.r@myfbm.com>
2026-08-06 05:49:47 +0530

    Fix SQL query syntax by removing duplicate 'FROM'

--- floorplan-to-structured-2d/helper.py
+++ floorplan-to-structured-2d/helper.py
@@ -759,12 +759,12 @@ async def trigger_email_notification(
 ):
     message = f"Plan: {plan_id} | Page Number: {page_number} | Extraction: {status}"
     query = f"""
-        SELECT project_name FROM FROM {credentials["CloudSQL"]["table_name_projects"]} WHERE LOWER(project_id) = LOWER(%s) 
+        SELECT project_name FROM {credentials["CloudSQL"]["table_name_projects"]} WHERE LOWER(project_id) = LOWER(%s) 
     """
     project_name = await run_in_threadpool(partial(pg_run, credentials, pg_pool, query, params=(project_id,), fetch=True))
     project_name = project_name[0]["project_name"]
     query = f"""
-        SELECT plan_name FROM FROM {credentials["CloudSQL"]["table_name_plans"]} WHERE LOWER(project_id) = LOWER(%s) AND LOWER(plan_id) = LOWER(%s)
+        SELECT plan_name FROM {credentials["CloudSQL"]["table_name_plans"]} WHERE LOWER(project_id) = LOWER(%s) AND LOWER(plan_id) = LOWER(%s)
     """
     plan_name = await run_in_threadpool(partial(pg_run, credentials, pg_pool, query, params=(project_id, plan_id,), fetch=True))
     plan_name = plan_name[0]["plan_name"]
