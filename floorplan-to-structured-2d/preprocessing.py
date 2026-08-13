45e750381514255cca756563f397b2089edcf481
Debajyoti-R <debajyoti.r@myfbm.com>
2026-08-14 03:45:24 +0530

    Update preprocessing.py

--- floorplan-to-structured-2d/preprocessing.py
+++ floorplan-to-structured-2d/preprocessing.py
@@ -43,16 +43,20 @@ def save(pdf_page, image_path_page):
 def to_sharp(image_path_page):
     image = cv2.imread(str(image_path_page))
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    gray = cv2.GaussianBlur(gray, (3, 3), 0)
 
-    clean = cv2.fastNlMeansDenoising(binary, h=30)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
-    sharpened = cv2.dilate(clean, kernel, iterations=1)
-    sharpened = cv2.erode(sharpened, kernel, iterations=1)
+    binary = cv2.adaptiveThreshold(
+        gray,
+        255,
+        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY,
+        31,
+        5,
+    )
 
     output_path = Path(image_path_page)
-    cv2.imwrite(output_path, sharpened)
-    return sharpened
+    cv2.imwrite(output_path, binary)
+    return binary
 
 def preprocess(pdf_path, page_index, maximum_dpi=400, minimum_dpi=250, image_path="/tmp/floor_plan.png"):
     image_path = Path(image_path)
